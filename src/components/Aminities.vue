d44cc11c78b9c52dd97f42f6bee23527709c378f
Sahil <smppatel999@gmail.com>
2022-03-25 10:45:06 +0530

    add img content

--- src/components/Aminities.vue
+++ src/components/Aminities.vue
@@ -29,8 +29,9 @@
     <div class="flex flex-wrap md:-m-2 -m-1">
       <div class="flex flex-wrap w-1/2">
         <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://img.staticmb.com/mbimages/project/Photo_h310_w462/2020/01/29/Project-Photo-9-Samyak-49-Ahmedabad-5132013_545_1050_310_462.jpg">
+          <img alt="gallery" class="w-full object-cover h-full object-center block transition duration-300" src="https://img.staticmb.com/mbimages/project/Photo_h310_w462/2020/01/29/Project-Photo-9-Samyak-49-Ahmedabad-5132013_545_1050_310_462.jpg">
         </div>
+
         <div class="md:p-2 p-1 w-1/2">
           <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://is1-3.housingcdn.com/012c1500/00e08a0fed7977a7becd8cbe001fbf7d/v0/medium.jpeg">
         </div>
@@ -55,25 +56,29 @@
 
 
 <section class="text-gray-600 body-font bg-white">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
+  <div class="container px-5 mx-auto">
+    <div class="flex flex-col text-center w-full">
       <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
     </div>
-    <div class="md:-m-2 -m-1">
-      <div class="">
-        <div class="md:p-4 p-2">
-          <div class="con">
-            <div class="dot dot1" title="Entrance"></div>
-            <div class="dot dot2"></div>
-            <div class="dot dot3"></div>
-            <img alt="gallery" class="mx-auto object-cover h-full object-center block" src="https://projectcdn.99acres.com/project_data/debd75/block2_96194/3D/47066_B2_1F1_3D.jpg">           
-          </div>
-        </div>
+  </div>
+</section>
+
+<section class="text-gray-600 body-font bg-white">
+  <div class="container mx-auto flex px-5 md:flex-row flex-col items-center">
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <img class="object-cover object-center rounded" alt="hero" src="https://cdn.shopify.com/s/files/1/2980/5140/products/Loomsolar1panelstand_180watts__1.jpg?v=1610514587">
+    </div>
+    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">We use Solar Panel
+        <!-- <br class="hidden lg:inline-block"> -->readymade gluten
+      </h1>
+      <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Gallery</button>
       </div>
     </div>
-<!--     <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button> -->
   </div>
 </section>
 
@@ -136,4 +141,15 @@
 img {
   width: 100%;
 }
+
+div[title]:hover::after {
+  content: attr(title);
+  position: absolute;
+  top: -100%;
+  background: red;
+  color: white;
+  padding: 3px;
+  left: 0;
+}
+
 </style>
