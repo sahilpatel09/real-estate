ccb28bc73aef9cb3b28010fc0f5d2270c4e1779f
Sahil <smppatel999@gmail.com>
2022-03-27 14:51:31 +0530

    Add content

--- src/components/Aminities.vue
+++ src/components/Aminities.vue
@@ -1,29 +1,9 @@
-<template><!-- 
-	<section class="text-gray-400 bg-gray-900 body-font">
-  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
-        <br class="hidden lg:inline-block">readymade gluten
-      </h1>
-      <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-      <div class="flex justify-center">
-        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-        <button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
-      </div>
-    </div>
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img class="object-cover object-center rounded box" alt="hero" src="@/assets/plan.png">
-    </div>
-  </div>
-</section> -->
-
-
-
+<template>
 
 <section class="text-gray-600 body-font bg-white">
   <div class="container px-5 py-20 mx-auto flex flex-wrap">
     <div class="flex w-full md:mb-20 mb-10 flex-wrap">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Master Cleanse Reliac Heirloom</h1>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4 mon uppercase">About Samyak 49</h1>
       <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
     </div>
 
--- src/components/aminities/BuildingImageWithText.vue
+++ src/components/aminities/BuildingImageWithText.vue
@@ -3,28 +3,18 @@
   <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
     <div>
       <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-        Brand new
+        Crafted and curated for the connoisseurs.
       </p>
     </div>
     <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-      <span class="relative inline-block">
-        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-          <defs>
-            <pattern id="679d5905-e08c-4b91-a66c-84aefbb9d2f5" x="0" y="0" width=".135" height=".30">
-              <circle cx="1" cy="1" r=".7"></circle>
-            </pattern>
-          </defs>
-          <rect fill="url(#679d5905-e08c-4b91-a66c-84aefbb9d2f5)" width="52" height="24"></rect>
-        </svg>
-        <span class="relative">The</span>
-      </span>
-      quick, brown fox jumps over a lazy dog
+The Exquisite Homes
     </h2>
-    <p class="text-base text-gray-700 md:text-lg">
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
+     <BuildingImage/>
+    <p class="text-base text-gray-700 md:text-lg mt-4">
+Samyak 49 has brought luxurious apartments in a prime location in navrangpura near CG road with multiple amenities with beautiful construction. Experienced each day at samyak49 with an elevated sense of belonging where each home is designed to be breathtakingly beautiful and offers an exhilaratingly jubilant life.  
     </p>
   </div>
- <BuildingImage/>
+
 
 </div>
 
