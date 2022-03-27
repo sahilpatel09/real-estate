05e37b4e2b54c89dfea90a743983eb0d2244ecb0
Sahil <smppatel999@gmail.com>
2022-03-27 14:55:36 +0530

    Add content

--- src/components/aminities/BuildingImageWithText.vue
+++ src/components/aminities/BuildingImageWithText.vue
@@ -3,18 +3,17 @@
   <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
     <div>
       <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-        Crafted and curated for the connoisseurs.
+        Crafted & curated for the connoisseurs.
       </p>
     </div>
     <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
 The Exquisite Homes
     </h2>
      <BuildingImage/>
-    <p class="text-base text-gray-700 md:text-lg mt-4">
+  </div>
+    <p class="text-base text-gray-700 md:text-lg mt-4 w-2/3 text-center mx-auto">
 Samyak 49 has brought luxurious apartments in a prime location in navrangpura near CG road with multiple amenities with beautiful construction. Experienced each day at samyak49 with an elevated sense of belonging where each home is designed to be breathtakingly beautiful and offers an exhilaratingly jubilant life.  
     </p>
-  </div>
-
 
 </div>
 
