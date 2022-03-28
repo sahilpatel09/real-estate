b1ac422420068f6e9b52c04977098b08e66d6480
Sahil <smppatel999@gmail.com>
2022-03-28 21:34:13 +0530

    Add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,14 +4,14 @@
   heroimage="living.jpeg"
   />
 
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-  <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+<div class="px-4 py-10 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div class="max-w-xl mb-1 md:mx-auto sm:text-center lg:max-w-2xl md:mb-1">
     <div>
-      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-        Brand new
+      <p class="inline-block px-3 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        APEXA GROUP
       </p>
     </div>
-    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+    <h2 class="max-w-lg font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
       <span class="relative inline-block">
         <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
           <defs>
@@ -23,25 +23,12 @@
         </svg>
         <span class="relative">The</span>
       </span>
-      quick, brown fox jumps over a lazy dog
+Hallmark of excellence
+
     </h2>
     <p class="text-base text-gray-700 md:text-lg">
       Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
     </p>
-  </div>
-  <div class="mx-auto lg:max-w-2xl">
-    <div class="relative w-full transition-shadow duration-300 hover:shadow-xl">
-      <img class="object-cover w-full h-56 rounded shadow-lg sm:h-64 md:h-80 lg:h-96" src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="" />
-      <a href="/" aria-label="Play Video" class="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 group hover:bg-opacity-25">
-        <div class="flex items-center justify-center w-16 h-16 transition duration-300 transform bg-gray-100 rounded-full shadow-2xl group-hover:scale-110">
-          <svg class="w-10 text-gray-900" fill="currentColor" viewBox="0 0 24 24">
-            <path
-              d="M16.53,11.152l-8-5C8.221,5.958,7.833,5.949,7.515,6.125C7.197,6.302,7,6.636,7,7v10 c0,0.364,0.197,0.698,0.515,0.875C7.667,17.958,7.833,18,8,18c0.184,0,0.368-0.051,0.53-0.152l8-5C16.822,12.665,17,12.345,17,12 S16.822,11.335,16.53,11.152z"
-            ></path>
-          </svg>
-        </div>
-      </a>
-    </div>
   </div>
 </div>
 
@@ -61,45 +48,66 @@
     <div class="flex flex-col justify-center">
       <div class="max-w-xl mb-6">
         <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-          The quick, brown fox<br class="hidden md:block" />
-          jumps over
-          <span class="relative px-1">
-            <div class="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400"></div>
-            <span class="relative inline-block text-deep-purple-accent-400">a lazy dog</span>
-          </span>
+          Multi-layered Expertise...
         </h2>
         <p class="text-base text-gray-700 md:text-lg">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
+          1. Best construction materials are used without compromising on the quality.<br/>
+          2. Well planned site layout ensures spacious and comfortable living.<br/>
+          3. Latest security systems to provide maximum safety to the residents.<br/>
+          4. Earthquake resistance RCC frame structure for safety against earthquake since Gujarat is located in a high seismic zone.<br/>
+          5. Beautifully landscaped gardens and sit out areas.<br/>
+          6. Stainless steel modern elevators.<br/>
+          <!-- 7. Recreational facilities including club houses and play areas.<br/>
+          8. High quality bathroom and sanitary fittings to match the best in its class.<br/>
+          9. Premium quality vitrified tiles are used to provide scratch resistance floors.<br/>
+          10. High standard electrical fittings and wiring to ensure high performance and top security.<br/>
+          11. Compliance of all legal obligations and ensure all papers are in place at the time of investment and handover. -->
         </p>
-      </div>
-      <div class="grid gap-5 row-gap-8 sm:grid-cols-2">
-        <div class="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
-          <div class="h-full p-5 border border-l-0 rounded-r">
-            <h6 class="mb-2 font-semibold leading-5">
-              I'll be sure to note that in my log
-            </h6>
-            <p class="text-sm text-gray-900">
-              Lookout flogging bilge rat main sheet bilge water nipper fluke to go on account heave down.
-            </p>
-          </div>
-        </div>
-        <div class="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
-          <div class="h-full p-5 border border-l-0 rounded-r">
-            <h6 class="mb-2 font-semibold leading-5">
-              A business big enough that it could be listed
-            </h6>
-            <p class="text-sm text-gray-900">
-              Those options are already baked in with this model shoot me an email clear.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
     <div>
-      <img class="object-cover w-full h-56 rounded shadow-lg sm:h-96" src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="" />
+      <img class="object-cover w-full h-56 rounded shadow-lg sm:h-96" src="https://apexa.in/assetsApexa/img/apexa/RealEstateSidebySide1.jpg" alt="" />
     </div>
   </div>
 </div>
+
+
+<div class="relative flex flex-col-reverse px-4 py-16 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full">
+  <div class="z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
+    <img src="https://apexa.in/assetsApexa/img/apexa/KhushrooImage.jpg" class="object-cover object-right w-full h-auto lg:w-auto lg:h-full" alt="" />
+  </div>
+  <div class="relative flex justify-end max-w-xl mx-auto xl:pr-32 lg:max-w-screen-xl">
+    <div class="mb-16 lg:pr-5 lg:max-w-lg lg:mb-0">
+      <div class="max-w-xl mb-6">
+        <div>
+          <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            RECENT PROJECT
+          </p>
+        </div>
+        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+Khushroo Residency
+        </h2>
+        <p class="text-base text-gray-700 md:text-lg">Envision an architectural gem of opulence and grace in the prime neighborhood. Clean architectural form that exudes a sense of quiet elegance and timeless grandeur. Inspired by the subtle touches of contemporary styling Khushroo Residency furthers the plush landscape surrounding it.
+
+Embrace this opportunity to own a rare residence of quality construction and a well planned campus life – the ultimate embodiment of your success.
+        </p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 <Footer/>
 </template>
 <script setup>
