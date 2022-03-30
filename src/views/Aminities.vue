3cadcdbbf2cca27deb070800e6580be840d32cf1
Sahil <smppatel999@gmail.com>
2022-03-30 12:53:32 +0530

    Add sections

--- src/views/Aminities.vue
+++ src/views/Aminities.vue
@@ -8,8 +8,36 @@
 
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
+    
+      <div class="mx-auto sm:text-center lg:max-w-7xl">
+    <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+      <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+        <span class="relative inline-block">
+          <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+            <defs>
+              <pattern id="5dc90b42-5ed4-45a6-8e63-2d78ca9d3d95" x="0" y="0" width=".135" height=".30">
+                <circle cx="1" cy="1" r=".7"></circle>
+              </pattern>
+            </defs>
+            <rect fill="url(#5dc90b42-5ed4-45a6-8e63-2d78ca9d3d95)" width="52" height="24"></rect>
+          </svg>
+          <span class="relative">The</span>
+        </span>
+        Specifications
+      </h2>
+      <p class="text-base text-gray-700 md:text-lg">
+The term &quot;privilege&quot; describes a condition of opulence and elegance. Samyak49 is developed with
+the intent of bringing grandeur in its finest, most basic form, combining elements of vitality, serenity,
+suppleness, and an open-air lifestyle.
+
+      </p>
+    </div>
+  </div>
+
+
     <div class="flex flex-wrap -m-4">
-      
+
+
 
       <div class="p-4 md:w-1/3" v-for="item in specificationData" :key="item">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
@@ -27,6 +55,70 @@
     </div>
   </div>
 </section>
+
+
+
+
+
+
+
+
+<section class="text-gray-600 body-font bg-white">
+  <div class="container px-5 mx-auto flex flex-wrap">
+
+    <div class="flex flex-wrap md:-m-2 -m-1 flex-col md:flex-row">
+      <div class="flex md:flex-wrap md:flex-row w-full flex-col md:w-1/2">
+
+        <div class="md:p-2 p-1 w-full md:w-1/2 image-card relative">
+          <img alt="gallery" class="w-full object-cover h-full object-center block transition duration-300" src="https://img.staticmb.com/mbimages/project/Photo_h310_w462/2020/01/29/Project-Photo-9-Samyak-49-Ahmedabad-5132013_545_1050_310_462.jpg">
+          <div class="bottom">Gym Area
+          </div>
+        </div>
+
+        <div class="md:p-2 p-1 w-full md:w-1/2 image-card relative">
+          <img alt="gallery" 
+          class="w-full object-cover h-full object-center" src="https://is1-3.housingcdn.com/012c1500/00e08a0fed7977a7becd8cbe001fbf7d/v0/medium.jpeg">
+          <div class="bottom">Dining
+          </div>
+        </div>
+        <div class="md:p-2 p-1 w-full image-card relative">
+          <img alt="gallery" class="w-full object-cover h-full object-center" src="https://newprojects.99acres.com/projects/unknown/samyak_49/images/gyoh97fz.jpg">
+          <div class="bottom">Playground
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-wrap w-full md:w-1/2 flex-col md:flex-row">
+        <div class="md:p-2 p-1 w-full relative image-card">
+          <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://newprojects.99acres.com/projects/unknown/samyak_49/images/gic02wqp_optOrig.jpg">
+          <div class="bottom">Dining Area
+          </div>
+        </div>
+        <div class="md:p-2 p-1 w-full md:w-1/2 image-card relative">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://teja12.kuikr.com/is/a/c/880x425/gallery_images/original/cf5d2d658495a7f.gif">
+          <div class="bottom">Entrance 
+          </div>
+        </div>
+        <div class="md:p-2 p-1 w-full md:w-1/2 image-card relative">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://is1-2.housingcdn.com/01c16c28/4ab28d8143ddc3f8d194fab77089d61f/v0/medium/3_bhk_apartment-for-sale-navrangpura-Ahmedabad-hall.jpg">
+          <div class="bottom">Living Room
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
 
 
 <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
@@ -77,15 +169,6 @@ Samyak 49 offers you a premium location, a luxurious lifestyle with all utilitie
 
 
     </div>
-    <p class="max-w-xl mb-4 text-base text-gray-700 sm:mx-auto">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris aliquip ex ea.
-    </p>
-    <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
-      Learn more
-      <svg class="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
-        <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z"></path>
-      </svg>
-    </a>
   </div>
 </div>
 
@@ -98,49 +181,6 @@ Samyak 49 offers you a premium location, a luxurious lifestyle with all utilitie
 
 
 
-
-
-<div class="relative flex bg-wine flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
-  <div class="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
-    <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-      <div class="max-w-xl mb-6">
-        <div>
-          <p class="inline-block mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full text-gray-100">
-            What we have used in your dream home
-          </p>
-        </div>
-        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none text-white">
-          Premium Range of Products
-        </h2>
-        <p class="text-base text-gray-200 md:text-lg">
-          The Samyak49 is a project that offers astoundingly promising golden prospects, ample connectivity,
-leisurely and peaceful leisure, long-term sustainability, and complementary financial appreciation.
-The skilled architects have developed an outstanding blend of contemporary style with certified
-luxury.
-        </p>
-      </div>
-      <div class="flex flex-col items-center md:flex-row">
-        <router-link
-          to="/contact-us"
-          class="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-indigo-600 hover:bg-indigo-900 focus:shadow-outline focus:outline-none"
-        >
-          Contact us
-        </router-link>
-      </div>
-
-    </div>
-  </div>
-  <div class="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
-    <video 
-    class=" w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
-    src="@/assets/about.mp4" autobuffer autoloop loop controls poster="@/assets/living.jpeg"></video>
-    <!-- <img
-      class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
-      src="@/assets/providers.jpg"
-      alt=""
-    /> -->
-  </div>
-</div>
 
 
         
@@ -334,6 +374,54 @@ Check out the Ground plan for the site.
 
 
 
+
+
+<div class="relative flex bg-wine flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
+  <div class="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
+    <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+      <div class="max-w-xl mb-6">
+        <div>
+          <p class="inline-block mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full text-gray-100">
+            What we have used in your dream home
+          </p>
+        </div>
+        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none text-white">
+          Premium Range of Products
+        </h2>
+        <p class="text-base text-gray-200 md:text-lg">
+          The Samyak49 is a project that offers astoundingly promising golden prospects, ample connectivity,
+leisurely and peaceful leisure, long-term sustainability, and complementary financial appreciation.
+The skilled architects have developed an outstanding blend of contemporary style with certified
+luxury.
+        </p>
+      </div>
+      <div class="flex flex-col items-center md:flex-row">
+        <router-link
+          to="/contact-us"
+          class="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-indigo-600 hover:bg-indigo-900 focus:shadow-outline focus:outline-none"
+        >
+          Contact us
+        </router-link>
+      </div>
+
+    </div>
+  </div>
+  <div class="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+    <video 
+    class=" w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+    src="@/assets/about.mp4" autobuffer autoloop loop controls poster="@/assets/living.jpeg"></video>
+    <!-- <img
+      class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+      src="@/assets/providers.jpg"
+      alt=""
+    /> -->
+  </div>
+</div>
+
+
+
+
+
 <div class="flex flex-col items-center justify-center max-w-2xl px-4 pt-16 mx-auto sm:max-w-xl md:max-w-2xl lg:pt-32 md:px-8">
   <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
     <div>
@@ -436,29 +524,30 @@ const specificationData = ref({
     data: "Franke SS Matt Finish Sink<br/>Kohler Premium Fittings<br/>Granite Platform<br/>Kota stone tack in store room."
   },
   2:{
-    img: require('@/assets/images/kitchen-area.jpeg'),
-    logo:require('@/assets/images/brands/franke.jpeg'),
+    img: require('@/assets/images/door.jpg'),
+    logo:require('@/assets/images/brands/aluk.jpeg'),
     title: "Doors & Windows",
-    data: "Franke SS Matt Finish Sink<br/>Kohler Premium Fittings<br/>Granite Platform<br/>Kota stone tack in store room."
+    data: "Ghana Sagvan Wood Frame<br/>Doors With Laminates<br/>Aluk Italian Aluminium Anodise<br/>Premium SS Heavy Door Hindges/Handles"
   },
   3:{
     img: require('@/assets/images/bathroom.png'),
     logo:require('@/assets/images/brands/kohler.png'),
-    title: "Bathrooms",
-    data: "Quotone Vitrified Tiles Dado upto Lintel Level<br/>Floor with Anti Skid Tiles "
+    title: "Bathrooms & Plumbing",
+    data: "Quotone Vitrified Tiles Dado upto Lintel Level<br/>Floor with Anti Skid Tiles<br/>Kohler Premium Series Fittings<br/>Kohler Premium Frameless Sanitaryware "
   },
   4:{
     img: require('@/assets/images/craptree.jpg'),
     logo:require('@/assets/images/brands/crabtree.jpeg'),
-    title: "Kitchen",
+    title: "CrabTree",
     data: "CrabTree Universal Type Switches"
   },
   5:{
-    img: require('@/assets/images/kitchen-area.jpeg'),
-    logo:require('@/assets/images/brands/franke.jpeg'),
-    title: "Kitchen",
-    data: "Franke SS Matt Finish Sink<br/>Kohler Premium Fittings<br/>Granite Platform<br/>Kota stone tack in store room."
+    img: require('@/assets/images/ac.jpg'),
+    logo:require('@/assets/images/brands/mitsubishi.jpeg'),
+    title: "Air Conditioner",
+    data: "Mitsubishi Heavy Industrial Split & VRF High wall Air Conditioner"
   },
+  
 
 })
 </script>
